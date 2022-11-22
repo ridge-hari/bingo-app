3836f3f1b23e657cb07d6b64493f5252b3e82ad3
ridge-hari <118787873+ridge-hari@users.noreply.github.com>
2022-11-22 18:08:39 +0900

    Update CurrentNumber.vue

--- src/pages/playground/components/CurrentNumber.vue
+++ src/pages/playground/components/CurrentNumber.vue
@@ -1,12 +1,17 @@
 <script setup lang="ts">
 defineProps<{
-  size: Number;
+  currentNum: Number;
 }>();
 </script>
 
 <template>
-  <div class="flex items-center justify-center rounded-lg bg-red-600 shadow-lg">
-    <span class="text-9xl text-black">?</span>
+  <div v-if="currentNum!=0" class="flex items-center justify-center rounded-lg bg-pink-700 shadow-lg">
+    <span class="font-serif text-9xl text-black mb-5">{{currentNum}}</span>
+  </div>
+  <div v-else class="flex items-center justify-center rounded-lg bg-yellow-600 shadow-lg">
+    <span class="font-serif text-9xl text-black mb-5">
+      <vue-feather type="smile" size="100"></vue-feather>
+    </span>
   </div>
 </template>
 
