85af37dc0ba918dfea0d4f45d8b976cf8c77a207
hari <ridgehari@gmail.com>
2022-11-24 05:38:26 +0900

    added number fetch animation

--- src/pages/playground/PlayGround.vue
+++ src/pages/playground/PlayGround.vue
@@ -15,28 +15,40 @@ const fetchableNumbers = ref(Array.from({length: row*col}, (_, i) => i + 1));
 const isEmpty = ref(false);
 const currentNum = ref(0);
 const previousNums = ref([0, 0, 0]);
+const isShowAnimation = ref(false);
+const isButtonDisabled = ref(false);
+
+const showAnimation = () => {
+  return new Promise( resolve => setTimeout(resolve, 2000) );
+}
 const onClickNextButton = () => {
-  console.log(fetchableNumbers.value);
   if(isEmpty.value) {
     alert("All the numbers has already been fetched!!! Please refresh the Game.");
     return;
   }
-  //get randow number
-  currentNum.value = fetchableNumbers.value[Math.floor(Math.random()*fetchableNumbers.value.length)];
-  //replace fetched number with zero
-  index.value = numbers.value.indexOf(currentNum.value);
-  numbers.value[index.value] = 0;
-  //delete current number from fetchable array
-  index.value = fetchableNumbers.value.indexOf(currentNum.value);
-  fetchableNumbers.value.splice(index.value, 1);
-  //disable next button if fetchable numbers empty
-  if(fetchableNumbers.value.length==0) {
-    isEmpty.value = true;
+  if (isButtonDisabled.value) {
+    return;
   }
-  //update previous numbers
-  previousNums.value[0] = previousNums.value[1];
-  previousNums.value[1] = previousNums.value[2];
-  previousNums.value[2] = currentNum.value;
+  isButtonDisabled.value=true;
+  (async () => {
+    isShowAnimation.value=true;
+    await showAnimation();
+    isShowAnimation.value=false;
+    //update previous numbers
+    previousNums.value[0] = previousNums.value[1];
+    previousNums.value[1] = previousNums.value[2];
+    previousNums.value[2] = currentNum.value;
+    //get randow number
+    currentNum.value = fetchableNumbers.value[Math.floor(Math.random()*fetchableNumbers.value.length)];
+    //delete current number from fetchable array
+    index.value = fetchableNumbers.value.indexOf(currentNum.value);
+    fetchableNumbers.value.splice(index.value, 1);
+    //disable next button if fetchable numbers empty
+    if(fetchableNumbers.value.length==0) {
+      isEmpty.value = true;
+    }
+    isButtonDisabled.value=false;
+  })();
 }
 const onClickRefreshButton = () => {
   router.go(0);
@@ -47,19 +59,19 @@ const onClickRefreshButton = () => {
   <div class="flex">
     <div class="w-1/2">
       <div class="flex justify-center items-center">
-        <PlayBoard :numbers="numbers" :row="row" :col="col" />
+        <PlayBoard :numbers="numbers" :row="row" :col="col" :fetchableNumbers="fetchableNumbers"/>
       </div>
     </div>
     <div class="w-1/2">
       <div class="flex justify-center items-center">
         <div>
-          <div class="bg-green-700 rounded-lg mb-12 drop-shadow-2xl">
-            <span class="font-serif text-4xl">Previous Numbers:</span>
+          <div class="bg-slate-700 rounded-lg mb-12 drop-shadow-2xl">
+            <span class="font-serif text-yellow-400 text-2xl">Previous Numbers:</span>
             <PreviousNumbers :previousNums="previousNums" />
           </div>
-          <div class="bg-green-700 mb-24 drop-shadow-2xl">
-            <span class="font-serif text-4xl">Current Number:</span>
-            <CurrentNumber :currentNum="currentNum" />
+          <div class="bg-slate-700 mb-24 drop-shadow-2xl">
+            <span class="font-serif text-yellow-400 text-4xl">Current Number:</span>
+            <CurrentNumber :currentNum="currentNum" :isShowAnimation="isShowAnimation"/>
           </div>
           <div class="grid grid-cols-2 grid-flow-col gap-4 my-8 h-10">
             <NextNumberButton :onclick="onClickNextButton " />
--- src/pages/playground/components/CurrentNumber.vue
+++ src/pages/playground/components/CurrentNumber.vue
@@ -1,15 +1,18 @@
 <script setup lang="ts">
 defineProps<{
-  currentNum: Number;
+  currentNum: Number,
+  isShowAnimation: Boolean,
 }>();
 </script>
 
 <template>
-  <div v-if="currentNum!=0" class="flex items-center justify-center rounded-lg bg-pink-700 shadow-lg">
-    <span class="font-serif text-9xl text-black mb-5">{{currentNum}}</span>
+  <div v-if="currentNum!=0" class="flex items-center justify-center rounded-lg bg-yellow-600 shadow-lg">
+    <span v-if="isShowAnimation" class="random font-serif text-8xl text-black mb-5"></span>
+    <span v-else class="font-serif text-9xl text-black mb-5">{{currentNum}}</span>
   </div>
   <div v-else class="flex items-center justify-center rounded-lg bg-yellow-600 shadow-lg">
-    <span class="font-serif text-9xl text-black mb-5">
+    <span v-if="isShowAnimation" class="random font-serif text-8xl text-black mb-5"></span>
+    <span v-else class="font-serif text-9xl text-black mb-5">
       <vue-feather type="smile" size="100"></vue-feather>
     </span>
   </div>
