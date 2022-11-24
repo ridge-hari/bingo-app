ccdc5371aaa45f84152151a0fb5a1ef12dd93c62
ridge-hari <118787873+ridge-hari@users.noreply.github.com>
2022-11-24 18:29:16 +0900

    updated layout

--- src/pages/playground/PlayGround.vue
+++ src/pages/playground/PlayGround.vue
@@ -77,12 +77,12 @@ const onClickRefreshButton = () => {
             <CurrentNumber :currentNum="currentNum" :isShowAnimation="isShowAnimation"/>
           </div>
           <div class="flex justify-between">
-            <button class="w-56 relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-yellow-600 to-slate-500 group-hover:from-yellow-600 group-hover:to-slate-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
+            <button class="w-56 relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-yellow-600 to-slate-500 group-hover:from-yellow-600 group-hover:to-slate-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-yellow-300 dark:focus:ring-yellow-500">
               <span class="w-56 relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
                 <NextNumberButton :onclick="onClickNextButton " />
               </span>
             </button>
-            <button class="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-red-600 to-slate-500 group-hover:from-red-600 group-hover:to-slate-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
+            <button class="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-red-600 to-slate-500 group-hover:from-red-600 group-hover:to-slate-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800">
               <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
                 <GameRefreshButton :onclick="onClickRefreshButton" />
               </span>
