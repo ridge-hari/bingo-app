12efee0a69d8f5d6d2f4a1e37eb90b498f7f41ab
ridge-hari <118787873+ridge-hari@users.noreply.github.com>
2022-11-24 13:32:21 +0900

    layout corrected

--- src/pages/playground/components/PreviousNumbers.vue
+++ src/pages/playground/components/PreviousNumbers.vue
@@ -4,10 +4,13 @@ defineProps<{
 }>();
 </script>
 <template>
-  <div class="grid grid-cols-3 grid-flow-row bg-slate-600 rounded-lg">
+  <div class="grid grid-cols-3 grid-flow-row bg-slate-600">
     <template v-for="i in 3" :key="i">
-      <div class="font-serif flex flex-col mx-10 my-8 w-16 text-pink-700">
-        <span class="text-4xl">{{ previousNums[i-1] }}</span>
+      <div class="font-serif flex flex-col mx-10 my-8 w-16 text-white">
+        <span class="text-xl text-white">{{ 4-i }}回前</span>
+        <vue-feather type="arrow-down" class="text-white ml-5" size="20"></vue-feather>
+        <span v-if="previousNums[i-1]!=0" class="text-2xl ml-5">{{ previousNums[i-1] }}</span>
+        <span v-else class="text-2xl ml-5">--</span>
       </div>
     </template>
   </div>
