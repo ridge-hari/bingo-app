ad8c411f57f08a9c073404130d4120237aea2e49
ridge-hari <118787873+ridge-hari@users.noreply.github.com>
2022-11-24 17:10:45 +0900

    layout updated

--- src/pages/playground/components/CurrentNumber.vue
+++ src/pages/playground/components/CurrentNumber.vue
@@ -6,14 +6,14 @@ defineProps<{
 </script>
 
 <template>
-  <div v-if="currentNum!=0" class="flex items-center justify-center bg-yellow-600 shadow-lg aspect-[3/1]">
+  <div v-if="currentNum!=0" class="flex items-center justify-center shadow-lg aspect-[3/1] ring-2 dark:ring-yellow-600">
     <span v-if="isShowAnimation" class="random font-serif text-8xl text-black"></span>
     <span v-else class="font-serif text-9xl text-black">{{ currentNum }}</span>
   </div>
-  <div v-else class="flex items-center justify-center bg-yellow-600 shadow-lg aspect-[3/1]">
+  <div v-else class="flex items-center justify-center shadow-lg aspect-[3/1] ring-2 dark:ring-yellow-600">
     <span v-if="isShowAnimation" class="random font-serif text-8xl text-black"></span>
     <span v-else class="font-serif text-9xl text-black">
-      --
+      <vue-feather type="dribbble" class="animate-bounce text-yellow-900" size="80"></vue-feather>
     </span>
   </div>
 </template>
