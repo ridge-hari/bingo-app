d56aa1624cdf840e3ffc6cfd5300fa2f16c1441a
hari <ridgehari@gmail.com>
2022-12-03 01:16:13 +0900

    update: playboard layout updated

--- src/pages/playground/components/PlayBoard.vue
+++ src/pages/playground/components/PlayBoard.vue
@@ -13,11 +13,11 @@ defineProps<{
     <table class="table-auto shadow-2xl bg-slate-600">
       <thead>
         <tr>
-          <th class="font-serif hover:animate-spin cursor-pointer text-4xl text-yellow-400 font-bold px-10 whitespace-nowrap">B</th>
-          <th class="font-serif hover:animate-spin cursor-pointer text-4xl text-yellow-400 font-bold px-10 whitespace-nowrap">I</th>
-          <th class="font-serif hover:animate-spin cursor-pointer text-4xl text-yellow-400 font-bold px-10 whitespace-nowrap">N</th>
-          <th class="font-serif hover:animate-spin cursor-pointer text-4xl text-yellow-400 font-bold px-10 whitespace-nowrap">G</th>
-          <th class="font-serif hover:animate-spin cursor-pointer text-4xl text-yellow-400 font-bold px-10 whitespace-nowrap">O</th>
+          <th class="font-serif hover:animate-spin cursor-pointer text-8xl text-yellow-400 font-bold px-10 whitespace-nowrap">B</th>
+          <th class="font-serif hover:animate-spin cursor-pointer text-8xl text-yellow-400 font-bold px-10 whitespace-nowrap">I</th>
+          <th class="font-serif hover:animate-spin cursor-pointer text-8xl text-yellow-400 font-bold px-10 whitespace-nowrap">N</th>
+          <th class="font-serif hover:animate-spin cursor-pointer text-8xl text-yellow-400 font-bold px-10 whitespace-nowrap">G</th>
+          <th class="font-serif hover:animate-spin cursor-pointer text-8xl text-yellow-400 font-bold px-10 whitespace-nowrap">O</th>
         </tr>
       </thead>
       <tbody>
@@ -25,12 +25,12 @@ defineProps<{
           <tr>
             <template v-for="c in col" :key="c">
               <td v-if="fetchableNumbers.includes(numbers[(r-1)*col+(c-1)])"
-                class="font-serif text-xl text-gray-400 font-medium px-10 pb-4 whitespace-nowrap"
+                class="font-serif text-xl text-gray-400 font-medium px-10 pb-4 whitespace-nowrap text-center"
               >
                 {{ isVisible?numbers[(r-1)*col+(c-1)]:"▬" }}
               </td>
               <td v-else
-                class="font-serif text-2xl text-yellow-400 font-medium px-10 pb-4 whitespace-nowrap"
+                class="font-serif text-2xl text-yellow-400 font-medium px-10 pb-4 whitespace-nowrap text-center"
               >
                 {{ numbers[(r-1)*col+(c-1)] }}
               </td>
